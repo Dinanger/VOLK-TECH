09f8844895ca24d4c4ce592dee5efd6abf912e05
dina <dinanyel01@hotmail.com>
2026-05-25 19:17:42 -0400

    first

---
+++ addons/control_stock_/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "order quantity control",
+    "summary": "order control to avoid order without stock",
+    "version": "1.0",
+    "depends": ["base", "sale_management"],
+    "installable": True
+}
